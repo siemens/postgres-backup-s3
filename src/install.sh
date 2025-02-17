83965b9ac311731dfb4d4096ba38f414eae801a2
Max Wittig <max.wittig@siemens.com>
2025-02-17 13:57:08 +0100

    chore: remove postgresql-client
    Co-authored-by: Fabio Huser <fh1ch@users.noreply.github.com>

--- src/install.sh
+++ src/install.sh
@@ -6,7 +6,7 @@ set -o pipefail
 apk update
 
 # install tools
-apk add postgresql-client gnupg python3 py3-pip aws-cli curl
+apk add gnupg python3 py3-pip aws-cli curl
 
 # install go-cron
 curl -L https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
